b3eb2a5ea9f58b408d72421780fe6ca6a2848f05
Leo4075 <leo.mollberg@gmail.com>
2026-01-12 13:23:00 +0100

    Start slider

---
+++ Programveckor26MarreUnity/Assets/Scenes/Leo/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Slider timerSlider;
+    public Timer timerText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
