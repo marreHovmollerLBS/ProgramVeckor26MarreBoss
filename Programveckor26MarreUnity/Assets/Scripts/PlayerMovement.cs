11fdb305e99a36a7ea864db59c690364ff1c4142
marreHovmollerLBS <marius.hovmoller@elev.ga.lbs.se>
2026-01-12 12:46:05 +0100

    Pytteliten förändring på spelaren

--- Programveckor26MarreUnity/Assets/Scripts/PlayerMovement.cs
+++ Programveckor26MarreUnity/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,14 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     public Vector2 Direction { get; private set; }
-    public float Speed { get; set; }
+    public float Speed;
     bool MovementAllowed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         Direction = Vector2.zero;
+        MovementAllowed = true;
     }
 
     // Update is called once per frame
