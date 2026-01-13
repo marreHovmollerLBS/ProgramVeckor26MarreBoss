5292d17ed8be0d20756aa775da51c56b9bd792b4
marreHovmollerLBS <marius.hovmoller@elev.ga.lbs.se>
2026-01-13 14:59:14 +0100

    Liten uppdatering Exitscript

--- Programveckor26MarreUnity/Assets/Scripts/Managers/ExitScript.cs
+++ Programveckor26MarreUnity/Assets/Scripts/Managers/ExitScript.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ExitScript : MonoBehaviour
 {
     public bool SwitchActivated;
     public bool SwitchAllowed;
+    public bool IsGoodDream;
+    TextMeshPro Text;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SwitchActivated = false;
         SwitchAllowed = true;
+        Text = "";
     }
 
     // Update is called once per frame
