3ed2ceaad9eae735d455bf9d5b1685a53509b73b
marreHovmollerLBS <marius.hovmoller@elev.ga.lbs.se>
2026-01-13 14:02:32 +0100

    Nytt exitscript i Marres

--- Programveckor26MarreUnity/Assets/Scripts/Managers/ExitScript.cs
+++ Programveckor26MarreUnity/Assets/Scripts/Managers/ExitScript.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitScript : MonoBehaviour
 {
+    public bool SwitchActivated;
+    public bool SwitchAllowed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SwitchActivated = false;
+        SwitchAllowed = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.E))
+        {
+            SwitchActivated = !SwitchActivated;
+            Debug.Log($"SwitchActivated is {SwitchActivated}");
+        }
+        if (SwitchActivated && SwitchAllowed)
+        {
+            SceneManager.LoadScene("Bedroom");
+        }
     }
 }
